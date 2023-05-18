ae0902e7f482267f93ae5721ec225d0a24881344
mariojoseee <mariojose.mj15@gmail.com>
2023-05-18 21:26:43 +0800

    catalog

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -1,50 +1,16 @@
-import Header from "@/components/Header";
 import Image from "next/image";
 import React from "react";
-import audi from "public/cars-logo/audi.png";
-import bmw from "public/cars-logo/bmw.png";
-import chevrolet from "public/cars-logo/chevrolet.png";
-import ferrari from "public/cars-logo/ferrari.png";
-import honda from "public/cars-logo/honda.png";
-import hyundai from "public/cars-logo/hyundai.png";
-import jaguar from "public/cars-logo/jaguar.png";
-import lamborghini from "public/cars-logo/lamborghini.png";
-import mazda from "public/cars-logo/mazda.png";
-import mercedes from "public/cars-logo/mercedes.png";
-import mitsubishi from "public/cars-logo/mitsubishi.png";
-import nissan from "public/cars-logo/nissan.png";
-import peugeot from "public/cars-logo/peugeot.png";
-import porsche from "public/cars-logo/porsche.png";
-import suzuki from "public/cars-logo/suzuki.png";
-import toyota from "public/cars-logo/toyota.png";
+import carsLogo from "public/data/carsLogo.json";
 
 function Catalog() {
 	return (
-		<>
-			{/* <div class="flex flex-wrap h-32 gap-16 justify-center items-center">
-				<div>
-					<Image class="h-28 w-auto rounded-lg" src={mazda} alt="" />
+		<div class="flex flex-wrap h-32 gap-16 justify-center items-center">
+			{carsLogo.map((v, i) => (
+				<div key={i}>
+					<Image width={500} height={500} class="h-20 w-auto" src={v.img} alt={v.name} />
 				</div>
-				<div>
-					<Image class="h-28 w-auto rounded-lg" src={audi} alt="" />
-				</div>
-				<div>
-					<Image class="h-28 w-auto rounded-lg" src={honda} alt="" />
-				</div>
-				<div>
-					<Image class="h-28 w-auto rounded-lg" src={bmw} alt="" />
-				</div>
-				<div>
-					<Image class="h-28 w-auto rounded-lg" src={mercedes} alt="" />
-				</div>
-				<div>
-					<Image class="h-28 w-auto rounded-lg" src={toyota} alt="" />
-				</div>
-				<div>
-					<Image class="h-28 w-auto rounded-lg" src={mitsubishi} alt="" />
-				</div>
-			</div> */}
-		</>
+			))}
+		</div>
 	);
 }
 
