f2339bef23751ce5de6c463c1d5e9d2ebf54ca69
mariojoseee <mariojose.mj15@gmail.com>
2023-05-18 17:23:22 +0800

    fix hscreen and add assets cars-logo

--- src/app/catalog/page.js
+++ src/app/catalog/page.js
@@ -1,12 +1,51 @@
 import Header from "@/components/Header";
+import Image from "next/image";
 import React from "react";
+import audi from "public/cars-logo/audi.png";
+import bmw from "public/cars-logo/bmw.png";
+import chevrolet from "public/cars-logo/chevrolet.png";
+import ferrari from "public/cars-logo/ferrari.png";
+import honda from "public/cars-logo/honda.png";
+import hyundai from "public/cars-logo/hyundai.png";
+import jaguar from "public/cars-logo/jaguar.png";
+import lamborghini from "public/cars-logo/lamborghini.png";
+import mazda from "public/cars-logo/mazda.png";
+import mercedes from "public/cars-logo/mercedes.png";
+import mitsubishi from "public/cars-logo/mitsubishi.png";
+import nissan from "public/cars-logo/nissan.png";
+import peugeot from "public/cars-logo/peugeot.png";
+import porsche from "public/cars-logo/porsche.png";
+import suzuki from "public/cars-logo/suzuki.png";
+import toyota from "public/cars-logo/toyota.png";
 
 function Catalog() {
-  return (
-    <>
-      <div>Catalog Page</div>
-    </>
-  );
+	return (
+		<>
+			{/* <div class="flex flex-wrap h-32 gap-16 justify-center items-center">
+				<div>
+					<Image class="h-28 w-auto rounded-lg" src={mazda} alt="" />
+				</div>
+				<div>
+					<Image class="h-28 w-auto rounded-lg" src={audi} alt="" />
+				</div>
+				<div>
+					<Image class="h-28 w-auto rounded-lg" src={honda} alt="" />
+				</div>
+				<div>
+					<Image class="h-28 w-auto rounded-lg" src={bmw} alt="" />
+				</div>
+				<div>
+					<Image class="h-28 w-auto rounded-lg" src={mercedes} alt="" />
+				</div>
+				<div>
+					<Image class="h-28 w-auto rounded-lg" src={toyota} alt="" />
+				</div>
+				<div>
+					<Image class="h-28 w-auto rounded-lg" src={mitsubishi} alt="" />
+				</div>
+			</div> */}
+		</>
+	);
 }
 
 export default Catalog;
--- src/components/PaymentType.js
+++ src/components/PaymentType.js
@@ -5,7 +5,7 @@ export default function PaymentType({ payment, image, name }) {
 	return (
 		<>
 			<div class="relative boxPayment">
-				<input type="radio" value={name} name={name} id={name} class="absolute inset-0 z-50 opacity-0 cursor-pointer" />
+				<input type="radio" value={name} name="paymentMethod" id={name} class="absolute inset-0 z-50 opacity-0 cursor-pointer" />
 				<label for={name} class="flex items-center justify-center gap-2 border border-grey rounded-3xl p-5 min-h-[80px] bg-white">
 					<Image src={image} alt="" />
 					<p class="text-sm lg:text-base font-semibold">{payment}</p>
